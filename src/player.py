fb07e5af41450dcd70a77825bac35faef88d0424
ronniedipple <ronniedipple@gmail.com>
2020-02-12 15:46:43 -0600

    Initial player class setup

--- src/player.py
+++ src/player.py
@@ -1,2 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+
+   def __init__(self, room, lives):
+       self.room = room
+       self.lives = lives
+
+
+
+def isPlayerAlive(self):
+   if (self.lives > 0):
+
+       return True
+
+       #print(f"You are alive, you have {self.lives} lives")
+   else:
+       return False
+
