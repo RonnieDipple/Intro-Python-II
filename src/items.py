7526a5b027100053fd54e135086a970c568cdc63
ronniedipple <ronniedipple@gmail.com>
2020-02-13 22:39:51 -0600

    Restructured item class, healing potion added, value added to items in preperation for a store

--- src/items.py
+++ src/items.py
@@ -1,42 +1,26 @@
-class Item:
-    # Base class
-    def __init__(self, name, description, value):
-        self.name = name
-        self.description = description
-        self.value = value
-
-    def __str__(self):
-        return "{}\n=====\n{}\Value: "
-
-
-class WitcherCoins(Item):
-    def __init__(self, amount, name, description, value):
-        self.amount = amount
-        super().__init__("WitcherCoins", "Coins to toss at your witcher".format(str(self.amount)), self.amount)
-
-
-class Weapon(Item):
+class Weapon:
 
     def __init__(self, name, description, value, damage):
         raise NotImplementedError(f"Do not create raw Weapon objects ")
-        self.damage = damage
-        super().__init__(name, description, value)
 
-        def __str__(self):
-            return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
+    def __str__(self):
+        return self.name
 
 
 class GesheftSilverSword(Weapon):
     def __init__(self):
-        super().__init__("Gesheft Silver Sword",
-                         "The Ultimate Weapon, with your bad breath being a close second",
-                         50,
-                         50)
+        self.name ="Gesheft Silver Sword"
+        self.description ="The Ultimate Weapon, with your bad breath being a close second",
+        self.damage = 50
+        self.value =50
 
 
 class EnchantedShotgun(Weapon):
     def __init__(self):
-        super().__init__("Enchanted Shotgun, Very powerful, If your name is Ash this must bring back memories")
+        self.name ="Enchanted Shotgun"
+        self.description = "Enchanted Shotgun, Very powerful, If your name is Ash this must bring back memories"
+        self.damage = 30
+        self.value = 100
 
 
 class Consumable:
@@ -53,3 +37,15 @@ class CrustyBread(Consumable):
     def __init__(self):
         self.name = "Crusty Loaf"
         self.healing_value = 10
+        self.value = 5
+
+class HealingPotion(Consumable):
+    def __init__(self):
+        self.name = "Healing Potion"
+        self.healing_value = 50
+        self.value = 60
+
+# class WitcherCoins(self):
+# def __init__(self, amount, name, description, value):
+# self.amount = amount
+# super().__init__("WitcherCoins", "Coins to toss at your witcher".format(str(self.amount)), self.amount)
--- src/player.py
+++ src/player.py
@@ -15,39 +15,10 @@ class Player:
         # players starting hp
         self.hp = 100
 
-        # Player attack method
+    def is_alive(self):
+        return self.hp > 0
 
-    def attack(self):
-        # Auto chooses best weapon
-        best_weapon = self.most_powerful_weapon()
-        room = world.tile_at(self.x, self.y)
-        enemy = room.enemy
-        print(f"You use{best_weapon.damage} against {enemy.name} ")
-        enemy.hp -= best_weapon.damage
-        if not enemy.is_alive():
-            print(f"You killed {enemy.name}")
-        else:
-            print(f"{enemy.name} HP is {enemy.hp}")
-
-    # x, y aka dx and dy correspond to coordinates on the map
-    # I felt more comfortable setting up movement using coordinates due to my Dyslexia
-    def move(self, dx, dy):
-        self.x += dx  # dx, dy represent named parameters
-        self.y += dy
-
-    def move_north(self):
-        self.move(0, -1)
-
-    def move_south(self):
-        self.move(0, 1)
-
-    def move_east(self):
-        self.move(1, 0)
-
-    def move_west(self):
-        self.move(-1, 0)
-
-    # prints inventory
+     # prints inventory
     def print_inventory(self):
         print("Inventory:")
         for item in self.inventory:
@@ -89,12 +60,48 @@ class Player:
         max_damage = 0
         best_weapon = None  # None is similar to null aka absence of value ake the abyss
         for item in self.inventory:
-            try:  # The inventory will sometimes have non-weapons they don't have an attribute for damage,
-                # so wrapping the code in a try and then error handling with AttributeError: pass which skips the comparison if
-                # and therefore prevents a AttributeError: 'str' object has no attribute 'damage' exception
+            # The inventory will sometimes have non-weapons they don't have an attribute for damage,
+            # so wrapping the code in a try and then error handling with AttributeError: pass which skips the comparison if
+            # and therefore prevents a AttributeError: 'str' object has no attribute 'damage' exception
+            try:
                 if item.damage > max_damage:
                     best_weapon = item
-                    max_damage = item.damage
+                max_damage = item.damage
             except AttributeError:
                 pass
+
         return best_weapon
+
+    # x, y aka dx and dy correspond to coordinates on the map
+    # I felt more comfortable setting up movement using coordinates due to my Dyslexia
+    def move(self, dx, dy):
+        self.x += dx  # dx, dy represent named parameters
+        self.y += dy
+
+    def move_north(self):
+        self.move(0, -1)
+
+    def move_south(self):
+        self.move(0, 1)
+
+    def move_east(self):
+        self.move(1, 0)
+
+    def move_west(self):
+        self.move(-1, 0)
+
+        # Player attack method
+
+    def attack(self):
+        # Auto chooses best weapon
+        best_weapon = self.most_powerful_weapon()
+        room = world.tile_at(self.x, self.y)
+        enemy = room.enemy
+        print(f"You use{best_weapon.damage} against {enemy.name} ")
+        enemy.hp -= best_weapon.damage
+        if not enemy.is_alive():
+            print(f"You killed {enemy.name}")
+        else:
+            print(f"{enemy.name} HP is {enemy.hp}")
+
+
